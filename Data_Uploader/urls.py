62001ee4993d868a59077c974a5c32ecbda946a5
HenryWashuHe <he.h@wustl.edu>
2025-07-10 16:19:30 +0800

    finalize the data cleaning and testing the download function

--- Data_Uploader/urls.py
+++ Data_Uploader/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-import Data_Uploader.views
-urlpatterns=[
-    path('Upload',Data_Uploader.views.get_upload_page)
+from Data_Uploader import views
+urlpatterns = [
+    path('Upload', views.get_upload_page, name='get_upload_page'),
+    path('download/', views.download_csv, name='download_csv'),
 ]
 
--- Data_Uploader/views.py
+++ Data_Uploader/views.py
@@ -21,8 +21,11 @@ def get_upload_page(request):
         df_3=pd.read_csv(io.StringIO(decoded_file_3))
         #keeping only rows that is in US or CN
         df_1=df_1[df_1['Country'].isin(['UNITED STATES', 'CANADA'])]
+        '''for i in reversed(range (len(df_1.loc[:,'Country']))):
+                    if not (df_1.loc[i,'Country'] == 'UNITED STATES' or df_1.loc[i,'Country']=='CANADA'):
+                        df_1.drop(i,inplace=True)'''
         #generating the set of order_number in file_2
-        completed=set(df_2['商城订单号'].astype(str).str[-5:].tolist())
+        completed=set(df_2['商城订单号'].astype(str).str[-5:])
         #loop through file_1 to delete the completed orders
         for i in reversed(range(len(df_1['OrderNumber']))):
             order_number = str(df_1.loc[i, 'OrderNumber'])[-5:]
@@ -31,16 +34,64 @@ def get_upload_page(request):
         new_rows=[]
         for i in reversed(range(len(df_1))):
             num=df_1.loc[i,'TotalItems']
-            if num >1:
+            if num>1:
                 row_to_duplicate = df_1.loc[[i]]
+                duplicates= pd.concat([row_to_duplicate] * (num - 1), ignore_index=True)
+                new_rows.append(duplicates)
+        if new_rows:
             df_1=pd.concat([df_1]+new_rows,ignore_index=True)
+        df_1=df_1.sort_values(by=['OrderID'], ascending=True)
+        df_1['OrderItems']=df_1['OrderItems'].str.split(';')
+        df_1 = df_1.explode('OrderItems')  # One row per item
+        df_1['Description'] = df_1['OrderItems'].str.split(':').str[-1].str.strip()
+        df_3 = df_3.rename(columns={'商品名称': 'Description'})  # 🔧 FIXED
+        df_1 = df_1.merge(df_3[['Description', 'Barcode', '可用库存（在库）']], on='Description', how='left')  # 🔧 FIXED
 
-        '''for i in reversed(range (len(df_1.loc[:,'Country']))):
-            if not (df_1.loc[i,'Country'] == 'UNITED STATES' or df_1.loc[i,'Country']=='CANADA'):
-                df_1.drop(i,inplace=True)'''
+        # new: compare qty with stock and flag warning
+        df_1['Qty'] = 1  # assume each exploded item = 1
+        df_1['Warning'] = df_1.apply(
+            lambda row: '⚠️库存不足' if pd.notnull(row['可用库存（在库）']) and row['Qty'] > row['可用库存（在库）'] else '',axis=1)
+        df_1['Shortage'] = df_1.apply(
+            lambda row: 'YES' if pd.notnull(row['ATP']) and row['Qty'] > row['ATP'] else '',
+            axis=1
+        )
+        df = pd.DataFrame()
+        df["*客户"] = ["CAMPBELL"] * len(df_merged)
+        df["*商城订单编号"] = df_merged["OrderNumber"].str.replace("_", "", regex=False)
+        df["平台"] = ["Others"] * len(df_merged)
+        df["平台店铺"] = ["" for _ in range(len(df_merged))]
+        df["*发货仓"] = ["LA-WLE1"] * len(df_merged)
+        df["*派送渠道编码"] = ["US_FEDEX_GROUND"] * len(df_merged)
+        df["加急类型"] = ["" for _ in range(len(df_merged))]
+        df["*签名服务"] = ["不需要签名"] * len(df_merged)
+        df["*保险服务"] = ["否"] * len(df_merged)
+        df["特殊备注"] = ["" for _ in range(len(df_merged))]
+        df["*收件人姓名"] = df_merged["FirstName"].fillna("") + " " + df_merged["LastName"].fillna("")
+        df["公司名称"] = ["不填"] * len(df_merged)
+        df["联系方式"] = df_merged["Phone"].fillna("")
+        df["*国家"] = df_merged["Country"].replace({"UNITED STATES": "US", "CANADA": "CA"}).fillna("")
+        df["*州"] = df_merged["County"].fillna("")
+        df["*城市"] = df_merged["Town"].fillna("")
+        df["*街道1"] = df_merged["Address1"].fillna("")
+        df["街道2"] = df_merged["Address2"].fillna("")
+        df["门牌号"] = df_merged["Address3"].fillna("")
+        df["*收件人邮编"] = df_merged["PostCode"].fillna("")
+        df["收件人邮箱"] = df_merged["Email"].fillna("")
+        df["*商品编码"] = df_merged["商品编码"].fillna("#NAME?")
+        df["*数量"] = ["1"] * len(df_merged)
 
-    return render(request,'Upload.html',{
-        'rows_1':rows_1,
-        'rows_2':rows_2,
-        'rows_3':rows_3
-    })# Create your views here.
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="2a_export.csv"'
+        df.to_csv(response, index=False, encoding="utf-8-sig")
+        return response
+
+
+    return render(request, "Upload.html.html")# Create your views here.
+def download_csv(request):
+    csv_data = request.session.get("csv_data")
+    if not csv_data:
+        return HttpResponse("No file prepared.", status=400)
+
+    response = HttpResponse(csv_data, content_type="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="2a_export.csv"'
+    return response
