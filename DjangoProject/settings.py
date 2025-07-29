57e6a5307ac5daa55597ce71cc972faaf19eb592
HenryWashuHe <he.h@wustl.edu>
2025-07-29 18:50:39 +0800

    change databases to Postgresql

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = "DjangoProject.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
